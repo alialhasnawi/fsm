31e130edfeb6f5196fe79900ef0ca24ba08c14bf
Ali Al-Hasnawi <44278853+alialhasnawi@users.noreply.github.com>
2021-12-25 13:07:52 -0500

    [Actions] Moved canvas state

--- src/components/canvas/canvas.tsx
+++ src/components/canvas/canvas.tsx
@@ -8,13 +8,13 @@ import { access, get_state, set_default_canvas, subscribe, unsubscribe } from ".
 import { CanvasRectangle, CanvasViewTransform, FSMContext, Point2D } from "../../types";
 import { DrawableElement } from "../elements/abstract";
 import { CANVAS_SIZE } from "../elements/constants";
-import { _CanvasTools } from "../tool_bar/tools";
 import { _CanvasHandler } from "./handlers";
+
+import './style.css';
 
 /** State only accessible to the canvas and it's handlers. */
 type CanvasRenderState = {
     caret_visible: boolean,
-    view_zone: CanvasViewTransform,
     selected_zone: CanvasRectangle | undefined,
 };
 
@@ -35,7 +35,6 @@ export class Canvas extends Component<any, CanvasRenderState> {
     public cursor: CursorData;
 
     private handlers: _CanvasHandler;
-    public tools: _CanvasTools;
 
     private caret_interval: number = -1;
 
@@ -43,12 +42,6 @@ export class Canvas extends Component<any, CanvasRenderState> {
         super();
         this.state = {
             caret_visible: false,
-
-            view_zone: {
-                zoom: 1,
-                x: 0,
-                y: 0,
-            },
             selected_zone: undefined,
         };
         this.cursor = {
@@ -59,7 +52,6 @@ export class Canvas extends Component<any, CanvasRenderState> {
             down: false,
             moving: false,
         };
-        this.tools = new _CanvasTools(this);
         this.handlers = new _CanvasHandler(this);
     }
 
@@ -73,8 +65,9 @@ export class Canvas extends Component<any, CanvasRenderState> {
         this.el!.onmouseup = this.handlers.mouse_up;
         this.el!.ondblclick = this.handlers.dbl_click;
         this.el!.oncontextmenu = this.handlers.prevent_default;
+        this.el!.onwheel = this.handlers.wheel;
 
-        subscribe(['nodes', 'links', 'active_objects', 'selected_object', 'temp_link'], this);
+        subscribe(['nodes', 'links', 'active_objects', 'selected_object', 'temp_link', 'view_zone'], this);
         access(this.draw);
     }
 
@@ -96,7 +89,7 @@ export class Canvas extends Component<any, CanvasRenderState> {
         );
     }
 
-    /**  */
+    /** Restart the caret blink interval. */
     reinit_caret() {
         if (get_state('selected_object') != null && this.caret_interval == -1) {
             this.caret_interval = window.setInterval(this.caret_function, 300);
@@ -115,8 +108,8 @@ export class Canvas extends Component<any, CanvasRenderState> {
     /**
      * Convert a mouse event to it's corresponding point in canvas draw space.
      */
-    public to_canvas_space(e: MouseEvent): Point2D {
-        const view = this.state.view_zone;
+    public event_to_canvas_space(e: MouseEvent): Point2D {
+        const view = get_state('view_zone');
         const rect = this.el!.getBoundingClientRect();
         const element_space = {
             x: e.clientX - rect.x,
@@ -130,33 +123,52 @@ export class Canvas extends Component<any, CanvasRenderState> {
         }
     }
 
+    /** Convert a point in canvas element space to it's corresponding point in canvas draw space. */
+    public point_to_canvas_space(p: Point2D): Point2D {
+        const view = get_state('view_zone');
+
+        // Apply the inverse of the view transform.
+        return {
+            x: (p.x - view.x) / view.zoom,
+            y: (p.y - view.y) / view.zoom,
+        }
+    }
+
     /** Draw the canvas using its own 2D context. */
     draw = () => {
+        const zone = get_state('view_zone');
+
         if (this.ctx != null) {
-            const zone = this.state.view_zone;
             this.ctx.setTransform(zone.zoom, 0, 0, zone.zoom, zone.x, zone.y);
-            this.draw_using(this.ctx);
+            this.draw_using(this.ctx, true);
         } else if (this.el != null) {
             const ctx = this.el.getContext("2d");
             if (ctx != null) {
                 this.ctx = ctx;
-                const zone = this.state.view_zone;
                 ctx.setTransform(zone.zoom, 0, 0, zone.zoom, zone.x, zone.y);
-                this.draw_using(ctx);
+                this.draw_using(ctx, true);
             }
         }
     }
 
-    draw_using(ctx: FSMContext) {
+    draw_using(ctx: FSMContext, with_border: boolean = false) {
         if (this.el != null) {
             const nodes = get_state("nodes");
             const links = get_state("links");
             const selected_object = get_state("selected_object");
             const current_link = get_state("temp_link");
 
-            ctx.clearRect(0, 0, this.el.width, this.el.height);
+            // Draw rectangle.
+            const upper_left = this.point_to_canvas_space({ x: 0, y: 0 });
+            const lower_right = this.point_to_canvas_space({ x: this.el.width, y: this.el.height });
+            ctx.clearRect(upper_left.x, upper_left.y, lower_right.x - upper_left.x, lower_right.y - upper_left.y);
             ctx.save();
-            ctx.translate(0.5, 0.5);
+
+            if (with_border) {
+                ctx.beginPath();
+                ctx.rect(-1, -1, this.el.width + 2, this.el.height + 2);
+                ctx.stroke();
+            }
 
             for (let i = 0; i < nodes.length; i++) {
                 ctx.lineWidth = 1;
@@ -174,7 +186,7 @@ export class Canvas extends Component<any, CanvasRenderState> {
                 current_link.draw(ctx, current_link == selected_object && this.state.caret_visible);
             }
 
-            // Debug.
+            // // Debug.
             // ctx.fillStyle = ctx.strokeStyle = 'magenta';
             // if (this.cursor.obj_at_mouse_down != null) this.cursor.obj_at_mouse_down.draw(ctx, false);
             // ctx.fillStyle = ctx.strokeStyle = 'lime';
